ba866e850167c8ad1c503db716868a4f17069738
Alisa Naik <alisanaik111@gmail.com>
2025-09-03 19:39:41 +0530

    Create robospeaker.py

---
+++ RoboSpeaker/robospeaker.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+
+print("Welcome to RoboSpeaker (Created by Alisa ❤)")
+
+while True:
+    x = input("Enter what you want me to speak (or 'q' to quit): ")
+
+    if x.lower() == "q":
+        tts = gTTS("Bye bye friends", lang='en')
+        tts.save("bye.mp3")
+        playsound("bye.mp3")
+        os.remove("bye.mp3")
+        break
+
+    tts = gTTS(x, lang='en')
+    tts.save("output.mp3")
+    playsound("output.mp3")
+    os.remove("output.mp3")
